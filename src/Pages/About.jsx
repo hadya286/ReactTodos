7563792bdd33387e95367ff045d999e2e72597fb
hadya286 <hadyamizari@gmail.com>
2022-11-27 21:13:18 +0200

    added close btn, integrated states with jsonserver

---
+++ src/Pages/About.jsx
@@ -0,0 +1,9 @@
+import {useContext} from "react"
+import ThemeContext from "../Context/themeContext"
+import {Link} from "react-router-dom"
+
+const About = () => {
+  return <Link>User1</Link>
+}
+
+export default About
